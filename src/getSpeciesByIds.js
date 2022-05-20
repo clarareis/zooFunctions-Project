39691c87aded1083be4eaf6ac2b4c9601bac2a03
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-05-20 18:24:20 -0300

    Conseguindo o 1º requisito depois da mentoria com o MD.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,30 +1,11 @@
 const data = require('../data/zoo_data');
+// console.log(data.species);
 
 function getSpeciesByIds(...ids) {
   if (ids.length === 0) {
     return ids;
   }
-  data.species.filter((ids2) => {
-    if (ids2.length >= 1) {
-      return ids2;
-    }
-    return ids2;
-  });
+  return data.species.filter((element) => ids.includes(element.id));
 }
-
-// especies.residents.name -> return em um [name especies]
-// destructuring
-
-// Retorne as seguintes informações do arquivo data:
-
-// Se a função receber apenas um id, retorne a espécie do animal referente a este id;
-
-// Se a função receber vários ids, retorne todas as espécies referente a esses ids.
-
-// O que será testado:
-
-// Ao receber como parâmetro um único id, retorna um array com a espécie referente a esse id;
-
-// Ao receber mais de um id, retorna um array com as espécies referentes aos ids.
 
 module.exports = getSpeciesByIds;
