f15a1706d1c8aa7ea417f1c3d9404db7f49aa092
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-05-20 18:32:27 -0300

    Explicando o que está acontecendo na função.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,11 +1,11 @@
 const data = require('../data/zoo_data');
 // console.log(data.species);
 
-function getSpeciesByIds(...ids) {
-  if (ids.length === 0) {
+function getSpeciesByIds(...ids) { // criando um array no parametro com o ... e juntando os elementos
+  if (ids.length === 0) { // se o não for passado nenhum parâmetro, retorna o array vazio []
     return ids;
   }
-  return data.species.filter((element) => ids.includes(element.id));
+  return data.species.filter((element) => ids.includes(element.id)); // o filter vai localizar no arquivo data, o objeto species e filtrar cada elemento deste objeto. retornando... se no meu array, estiver incluído === ao elementopassado.id (do objeto) === true e então ele irá incluir no array com a função filter.
 }
 
 module.exports = getSpeciesByIds;
