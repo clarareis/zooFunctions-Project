7465224e3f1e42166f9c73dabc1b6f93246bf047
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-05-23 16:38:27 -0300

    Conseguindo resolver o 2º requisito com a mentoria.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,15 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-
+  return data.species.find((element) => element.name === animal)
+    .residents
+    .every((objectAnimal) => objectAnimal.age >= age);
 }
-// como que eu vejo todos os animais? for = map/each
+
+// getAnimalsOlderThan()
 
 module.exports = getAnimalsOlderThan;
-/* // Ao receber uma espécie e uma idade como parâmetro, retorne se todos os animais dessa espécie possuem essa idade ou são mais velhos.
-1 - Pegar o animal passado (pegar a idade dele residents.age) e comparar com os outros animais da espécie para verificar se a idade é = ou >
-// Verifique se todos os animais da espécie passada como parâmetro possuem a idade mínima:
-// Os animais devem ter essa idade ou serem mais velhos.
-// Retorne um valor booleano.
-// O que será testado:
-// Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada */
