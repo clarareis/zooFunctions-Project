89b59322cf26301b49a5d97919e2e291c5204e5b
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-05-23 16:52:18 -0300

    Verificando os erros do lint do requisito 5.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,18 +3,18 @@ const data = require('../data/zoo_data');
 // como faço para somar todos os animais?
 
 function countAnimals(parameter) {
-  const residentes = data.species;
-  let counts = 0;
-  for (let index = 0; index < residentes.length; index += 1) {
-    if (residentes[index].name === parameter) {
-     counts.push += 1;
-    }
-  }
+//   const residentes = data.species;
+//   let counts = 0;
+//   for (let index = 0; index < residentes.length; index += 1) {
+//     if (residentes[index].name === parameter) {
+//      counts.push += 1;
+//     }
+//   }
 }
 /* data.species.map((element, index) => {
   if (parameter === undefined) {
     return console.log(element.residents.name);
   }
   return index; */
-countAnimals({ specie: 'penguins' });
+// countAnimals({ specie: 'penguins' });
 module.exports = countAnimals;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  
+  data.employees.find((getPerson) => getPerson.firstName === employeeName);
 }
 
 module.exports = getEmployeeByName;
