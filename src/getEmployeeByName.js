7a98691c25807956fe2a5f533ea2e3649e49cf9a
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-05-24 16:30:10 -0300

    Conseguindo resolver o 3 requisito, na mentoria com o Rennan, ele me fez ver que eu estava usando o reduce e no questionamento vi que deveria ser o find.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  data.employees.find((getPerson) => getPerson.firstName === employeeName);
+  // eslint-disable-next-line no-unused-expressions
+  // const getName = data.species;
+  if (employeeName === undefined) {
+    return {};
+  }
+  return data.employees.find((element) =>
+    element.firstName === employeeName || element.lastName === employeeName);
 }
+console.log(getEmployeeByName('Nigel'));
 
+getEmployeeByName('Nigel');
 module.exports = getEmployeeByName;
+// => getPerson.firstName === employeeName;
