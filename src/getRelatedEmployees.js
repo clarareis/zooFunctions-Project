c64a9a8912b6673834036348c4347a6feddb00d3
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-05-24 17:05:45 -0300

    Tentando resolver o 4 requisito.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,13 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.filter((element) => element.managers)
+    .find((manager) => manager === id);
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
 }
+console.log(isManager('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 
 module.exports = { isManager, getRelatedEmployees };
