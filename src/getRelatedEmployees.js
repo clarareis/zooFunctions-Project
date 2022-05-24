48ec585f139ed6e78c39b08f4c4a95cacf921c0e
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-05-24 20:08:43 -0300

    Conseguindo concluir o 4 requisito.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -5,8 +5,22 @@ function isManager(id) {
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const empregados = data.employees.filter((element) =>
+      element.managers.includes(managerId));
+    return empregados.map((name) =>
+      `${name.firstName} ${name.lastName}`);
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+
+/* function getRelatedEmployees(managerId) {
+  if (isManager(managerId) === true) {
+    data.employees.filter((element) =>
+      element.managers.includes(managerId)).map((name) =>
+      `'${name.firstName} ${name.lastName}'`);
+  }
+} */
+// console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+// console.log(getRelatedEmployees('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 
 module.exports = { isManager, getRelatedEmployees };
