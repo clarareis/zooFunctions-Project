ce5d290b079b88a91ae01d8be2fa97ad4e503713
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-05-25 11:33:48 -0300

    Resolvendo o requisito 5.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,20 +1,26 @@
 const data = require('../data/zoo_data');
 
-// como faço para somar todos os animais?.
-
 function countAnimals(parameter) {
-//   const residentes = data.species;
-//   let counts = 0;
-//   for (let index = 0; index < residentes.length; index += 1) {
-//     if (residentes[index].name === parameter) {
-//      counts.push += 1;
-//     }
-//   }
-}
-/* data.species.map((element, index) => {
   if (parameter === undefined) {
-    return console.log(element.residents.name);
+    const numbersOfResidents = data.species.map((element) =>
+      ({ [element.name]: element.residents.length }));
+    const newObject = Object.assign(...numbersOfResidents);
+    return newObject;
   }
-  return index; */
-// countAnimals({ specie: 'penguins' });
+  const { specie, sex } = parameter;
+  if (sex) {
+    return data.species.find((especies) => especies.name === specie).residents
+      .filter(({ sex: sex2 }) => sex2 === sex).length;
+  }
+  return data.species.find((nameAnimal) => nameAnimal.name === specie).residents.length;
+}
+
+console.log(countAnimals({ specie: 'elephants' }));
+
 module.exports = countAnimals;
+
+/* { specie: 'elephants', sex: 'male' }
+{ specie: 'penguins' }
+
+/*   Retorne a quantidade de animais residentes no zoológico da espécie passada por parâmetro. Por exemplo:
+*/
