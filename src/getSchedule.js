f2f350df715723489fedb1c48a5660089b0912a9
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-05-25 20:04:44 -0300

    Tentando seguir o requisito 9.

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,11 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  
+  if (scheduleTarget === species.name) {
+    species.map((element) => element.availability);
+  }
+  return scheduleTarget;
 }
 
-module.exports = getSchedule;
+console.log(getSchedule('lions'));
+// Retorne um array com os dias da semana em que um animal está disponível para visitação caso o parâmetro da função seja um animal. Por exemplo: [ 'Tuesday', 'Thursday', 'Saturday', 'Sunday' ];
