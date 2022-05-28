5397d66e04c29631726478b1df48dd6bfaee4d9c
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-05-28 13:16:21 -0300

    Conseguindo resolver o 10 requisito em 2horas :rocket:

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-
+  const getSpecie = employees.find((person) => person.id === id).responsibleFor
+    .find((element) => element[0]);
+  const animal = species.find((animals) => animals.id === getSpecie).residents;
+  const oldestAnimal = animal.reduce((acc, curr) => ((acc.age > curr.age) ? acc : curr));
+  return Object.values(oldestAnimal);
 }
+
+console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 
 module.exports = getOldestFromFirstSpecies;
