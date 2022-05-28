2ac56b6e577e1079ec5d6e83ae1815b63334cfed
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-05-28 16:12:02 -0300

    Iniciando o 12º requisito.

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,26 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+const hours = {
+  Tuesday: { open: 8, close: 6 },
+  Wednesday: { open: 8, close: 6 },
+  Thursday: { open: 10, close: 8 },
+  Friday: { open: 10, close: 8 },
+  Saturday: { open: 8, close: 10 },
+  Sunday: { open: 8, close: 8 },
+  Monday: { open: 0, close: 0 },
+};
+
+describe('Testes da função getOpeningHours', () => {
+  it('Testando a função quando não recebe parâmetros', () => {
+    expect(getOpeningHours()).toEqual(hours);
+  });
+  it('Testando a função quando recebe "Monday", retorna a mensagem que o zoológico está fechado', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+  it('Testando a função quando recebe "Tuesday", "09:00-AM", retorna a mensagem que o zoológico está aberto', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+  it('Testando a função quando recebe "Wednesday", "09:00-PM", retorna a mensagem que o zoológico está fechado', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+  });
+});
