f71bdb67fb4cbd5f9f79baea2316e3b21f55b54a
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-05-30 11:22:51 -0300

    Tentando solucionar o 11º requisito.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,17 +1,16 @@
 const { employees } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
 
-// const information = employees;
-
+const functionForParameterGetId = (id) => employees.find((idElement) => idElement).id;
+const functionForParameterGetName = (name) => employees.find((nameElement) => nameElement.name === name);
+console.log(functionForParameterGetName('Nigel'));
 function getEmployeesCoverage(info) {
-  return employees.find((get) => (get.firstName === info
-    || get.lastName === info || get.id === info));
+
 }
 
-console.log(getEmployeesCoverage('Nelson'));
+// console.log(getEmployeesCoverage('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 module.exports = getEmployeesCoverage;
 
-/* [ sem parametros
+/* [ sem parametros retorna todos os funcionários, com o parâmetro, localiza o funcionário com as informações abaixo.
 {
 "id":  "c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1",
 "fullName":  "Nigel Nelson",
